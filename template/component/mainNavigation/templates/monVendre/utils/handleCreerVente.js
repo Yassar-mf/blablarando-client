90ecda3348038465e9db7b78445103bbd7c41d5e
Yassar-mf <66548030+Yassar-mf@users.noreply.github.com>
2025-01-16 13:51:52 +0000

    commit 2

--- template/component/mainNavigation/templates/monVendre/utils/handleCreerVente.js
+++ template/component/mainNavigation/templates/monVendre/utils/handleCreerVente.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
-const url = 'https://kilo-kello-4d8ce17ec2a1.herokuapp.com/creerVente'
+const url = 'https://blablarando.replit.app/creerVente'
 const handleCreerVente = async (navigation,data) =>{
     console.log('vendreeeeeeeeeeeeee',data)
-    //const response = await axios.post(url,{data})
-    //const succes = response.data
-    //navigation.navigate('Vente cree',{data})
+    const response = await axios.post(url,{data})
+    navigation.navigate('Vente cree',{data})
 }
 
 export default handleCreerVente;
